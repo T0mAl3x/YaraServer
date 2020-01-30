13f791537734efac8c1879566d5b7602102222f4
Tom <alexandru.tomei@gmail.com>
2020-01-30 16:14:27 +0200

    Initial commit

---
+++ YaraServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YaraServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
